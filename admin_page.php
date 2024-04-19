0675a6728367fcaf42b0aa7c41c1c305a2ff6880
Iraklis Schizas <102415348+IraklisSchizas@users.noreply.github.com>
2024-04-19 19:01:54 +0300

    Update admin_page.php

--- admin_page.php
+++ admin_page.php
@@ -31,8 +31,6 @@ if(!isset($_SESSION['user_name'])){
       <h3>Γειά σου, <span>admin</span></h3>
       <h1>Καλωσήρθες <span><?php echo $_SESSION['user_name'] ?></span>!</h1>
       <p>αυτή είναι η σελίδα του διαχειριστή</p>
-      <!-- <a href="login.php" class="btn">login</a>
-      <a href="register_form.php" class="btn">register</a> -->
       <a href="register_rescuer.php" class="btn">Εγγραφή Διασώστη</a>
       <a href="manage_database.php" class="btn">Διαχείρηση Βάσης</a>
       <a href="logout.php" class="btn">αποσύνδεση</a>
