4b056a37f8a1ab83c5bb23f6d8d3136fed7b4983
Iraklis Schizas <hraklhs.sx@outlook.com>
2024-04-22 22:59:21 +0300

    Update load_tables.php

--- load_tables.php
+++ load_tables.php
@@ -1,14 +1,12 @@
 <?php
 @include 'config.php';
 
-// Εκτέλεση ερωτήματος SQL για τα δεδομένα του πίνακα items
 $sql_items = "SELECT * FROM items";
 $result_items = $conn->query($sql_items);
 
-// Έλεγχος για τα δεδομένα του πίνακα items
 if ($result_items->num_rows > 0) {
-    // Αρχείο HTML πίνακα για τα δεδομένα του πίνακα items
     echo "<table>";
+    echo "<tr><th colspan='4'>Items</th></tr>"; // Header για τον πίνακα Items
     echo "<tr><th>ID</th><th>Name</th><th>Category</th><th>Details</th></tr>";
     while($row = $result_items->fetch_assoc()) {
         echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["category"]."</td><td>".$row["details"]."</td></tr>";
@@ -18,14 +16,12 @@ if ($result_items->num_rows > 0) {
     echo "0 results";
 }
 
-// Εκτέλεση ερωτήματος SQL για τα δεδομένα του πίνακα categories
 $sql_categories = "SELECT * FROM categories";
 $result_categories = $conn->query($sql_categories);
 
-// Έλεγχος για τα δεδομένα του πίνακα categories
 if ($result_categories->num_rows > 0) {
-    // Αρχείο HTML πίνακα για τα δεδομένα του πίνακα categories
     echo "<table>";
+    echo "<tr><th colspan='2'>Categories</th></tr>"; // Header για τον πίνακα Categories
     echo "<tr><th>ID</th><th>Name</th></tr>";
     while($row = $result_categories->fetch_assoc()) {
         echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td></tr>";
