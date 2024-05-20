4f7e117f51b5b71568aa476d46df7923144d216d
Iraklis Schizas <hraklhs.sx@outlook.com>
2024-05-20 16:03:36 +0300

    Update display.php

--- display.php
+++ display.php
@@ -20,7 +20,39 @@ if (!isset($_SESSION['user_name'])) {
 <body>
     <div class="form-container">
         <form action="" method="post">
-            <p><a href="add_to_database.php" class="btn">Προσθήκη νέου Αντικειμένου ή Κατηγορίας</a></p>
+            <button class="form-btn" onclick="window.location.href = 'add_to_database.php'">Προσθήκη Αντικειμένου</button>
+            <br><br>
+            <h2>Items Table</h2>
+            <table class="table" id="jsonItemsTable">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                </tbody>
+                </table><br>
+            <p><a href="admin_page.php">Πίσω στη σελίδα Διαχειριστή</a></p>
         </form>
     </div>
 </body>
