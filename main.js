3bd1cf09ce324a197e96a59097827b736c1392df
Iraklis Schizas <hraklhs.sx@outlook.com>
2024-05-21 10:19:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -112,14 +112,3 @@ function populateCategoriesTable(categories) {
     row.innerHTML = `<td>${category.id}</td><td>${category.name}</td>`;
   });
 };*/
-
-const getCurrentDateTime = () => {
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
-  const day = ('0' + currentDate.getDate()).slice(-2);
-  const hours = ('0' + currentDate.getHours()).slice(-2);
-  const minutes = ('0' + currentDate.getMinutes()).slice(-2);
-  const seconds = ('0' + currentDate.getSeconds()).slice(-2);
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-};
