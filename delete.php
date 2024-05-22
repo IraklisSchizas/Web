5f8f85b9402c47ab1b6e6fbf66e90d5a09bd08cb
Iraklis Schizas <hraklhs.sx@outlook.com>
2024-05-22 18:29:38 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -9,13 +9,27 @@ if (!isset($_SESSION['user_name'])) {
 
 if (isset($_GET['deleteid'])){
     $id = $_GET['deleteid'];
-
-    $sql = "DELETE FROM items WHERE id=$id";
-    $result = mysqli_query($conn, $sql);
-    if($result) {
-        header("Location: display.php");
+    if (isset($_GET['is_a'])){
+        $is_a = $_GET['is_a'];
+        if ($is_a == 'item'){
+            // Εντολή διαγραφής για τα αντικείμενα
+            $sql = "DELETE FROM items WHERE id=$id";
+        }elseif ($is_a == 'category'){
+            // Εντολή διαγραφής για τις κατηγορίες
+            $sql = "DELETE FROM categories WHERE id=$id";
+        }else{
+            die(mysqli_error($conn));
+        }
+        $result = mysqli_query($conn, $sql);
+        if($result) {
+            header("Location: display.php");
+        }else{
+            die(mysqli_error($conn));
+        }
     }else{
         die(mysqli_error($conn));
     }
-} 
+}else{
+    die(mysqli_error($conn));
+}
 ?>
