34c8536e91b320d9b07aff8352a0749fd04f4b7f
XouselasNikolaos <85506620+XouselasNikolaos@users.noreply.github.com>
2024-05-24 14:12:20 +0300

    .

--- civilian_announcements.php
+++ civilian_announcements.php
@@ -14,11 +14,12 @@ if (!isset($_SESSION['user_name'])) {
 <!DOCTYPE html>
 <html lang="el">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ανακοινώσεις</title>
     <link rel="stylesheet" href="css/style.css">
+</head>>
 </head>
 <body>
     <div class="form-container">
