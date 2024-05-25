99abff188e2137ea611c72b5504c2add06bd9a5d
Iraklis Schizas <hraklhs.sx@outlook.com>
2024-05-25 09:30:15 +0300

    Προσθήκη στήλης item_ids στον πίνακα annaouncements
    Προστέθηκε νέα στήλη, προκειμένου να συσχετίζονται items με κάθε ανακοίνωση, και έτσι να μπορεί ο πολίτης να κάνει αντίστοιχα offers.

--- user_db.sql
+++ user_db.sql
@@ -32,6 +32,7 @@ CREATE TABLE `announcements` (
   `title` varchar(255) NOT NULL,
   `details` text NOT NULL,
   `date` datetime(6) NOT NULL,
+  `item_ids` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
