b8e88426bb03904a86ca23e046d48276c590b1d4
Thanos Kapnias <thanoskapnias@gmail.com>
2024-05-26 18:47:35 +0300

    .
    .

--- admin_map.php
+++ admin_map.php
@@ -1,66 +1,71 @@
 <?php
-
 @include 'config.php';
 
 session_start();
-// Ελέγχουμε αν ο χρήστης είναι συνδεδεμένος
+
 if (!isset($_SESSION['user_name'])) {
     header('location:login.php');
     exit();
 }
 
 // SQL query to get vehicle data
-$sql = " SELECT id, username, latitude, longitude FROM users WHERE user_type='rescuer' ";
+$sql = "SELECT id, username, latitude, longitude FROM users WHERE user_type='rescuer'";
 $result = mysqli_query($conn, $sql);
 
 $vehicles = array();
 
 if ($result->num_rows > 0) {
-    // Fetch all vehicle data
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         $vehicles[] = $row;
     }
 } else {
-    echo json_encode(["error" => "No vehicles found"]);
-    exit;
-}
-//Απεικόνιση σε μορφή JSON για να μπορεί να τα απεικονίσει η JavaScript
-header('Content-Type: application/json');
-    echo json_encode($vehicles);
+    echo "No vehicles found";
     exit();
+}
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="el">
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Χάρτης Διαχειριστή</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Χάρτης Διαχειριστή</title>
 
-   <!-- Leaflet CSS and JS -->
-   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <!-- Leaflet CSS and JS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 
-   <!-- Custom CSS file link -->
-   <link rel="stylesheet" href="css/style.css">
+    <!-- Custom CSS file link -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="container">
-   <div class="content">
-      <h3>Χάρτης Διαχειριστή</h3>
-      <br>
-      <!-- Leaflet Map Container -->
-      <div id="map" style="height: 600px; width: 1000px;"></div>
-      <br>
-      <a href="admin_page.php" class="btn">Πίσω στη σελίδα Διαχειριστή</a>
-   </div>
-</div>
+    <div class="container">
+        <div class="content">
+            <h3>Χάρτης Διαχειριστή</h3>
+            <br>
+            <!-- Leaflet Map Container -->
+            <div id="map" style="height: 600px; width: 1000px;"></div>
+            <br>
+            <a href="admin_page.php" class="btn">Πίσω στη σελίδα Διαχειριστή</a>
+        </div>
+    </div>
 
+    <script>
+        // Δημιουργία του χάρτη
+        var map = L.map('map').setView([37.9838, 23.7275], 13);
 
-<script src="vehicle.js"></script>
+        // Προσθήκη του βασικού layer από το OpenStreetMap
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // Προσθήκη markers για κάθε όχημα
+        <?php foreach ($vehicles as $vehicle): ?>
+            var marker = L.marker([<?php echo $vehicle['latitude']; ?>, <?php echo $vehicle['longitude']; ?>]).addTo(map);
+            marker.bindPopup("<b><?php echo $vehicle['username']; ?></b><br>Latitude: <?php echo $vehicle['latitude']; ?><br>Longitude: <?php echo $vehicle['longitude']; ?>");
+        <?php endforeach; ?>
+    </script>
 </body>
 </html>
 
