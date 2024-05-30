6d8d029c504d3082db6002f346895df2168acdd5
XouselasNikolaos <85506620+XouselasNikolaos@users.noreply.github.com>
2024-05-30 22:34:48 +0300

    up

--- cargo_management.php
+++ cargo_management.php
@@ -28,7 +28,7 @@ if (isset($_POST['unload_items'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Διαχείριση Φορτίου</title>
