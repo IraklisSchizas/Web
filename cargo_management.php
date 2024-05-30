aab36b291bbc7e2eca3d507634ef7a1b82266b93
XouselasNikolaos <85506620+XouselasNikolaos@users.noreply.github.com>
2024-05-30 22:04:05 +0300

    css στο cargo

--- cargo_management.php
+++ cargo_management.php
@@ -32,6 +32,7 @@ if (isset($_POST['unload_items'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Διαχείριση Φορτίου</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h2>Διαχείριση Φορτίου</h2>
