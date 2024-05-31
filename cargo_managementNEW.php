eccc7b81b730f86a66f776ed82cf057745a2845f
XouselasNikolaos <85506620+XouselasNikolaos@users.noreply.github.com>
2024-05-31 17:39:29 +0300

    update

--- cargo_managementNEW.php
+++ cargo_managementNEW.php
@@ -30,8 +30,33 @@ if (isset($_POST['unload_items'])) {
     }
 }
 
-function loadItemsFromDatabase() {
-    global $conn;
+// Κώδικας επιλογής δεδομένων οχημάτων, αιτημάτων, προσφορών κλπ.
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Διαχείριση Φορτίου</title>
+    <link rel="stylesheet" href="css/style.css">
+   
+    
+
+</head>
+<body>
+    <div class="form-container">
+        <form id="initialize_form" action="" method="post">
+            <p><a href="rescuer_page.php">Πίσω στη σελίδα Διασώστη</a></p>
+            <br><br>            
+            <h2>Cargo Management</h2><br>
+
+
+<?php
+    function loadItemsFromDatabase() {
+           global $conn;
 
     // Επιλέγουμε τα αντικείμενα προς φόρτωση από τη βάση
     $result = mysqli_query($conn, "SELECT * FROM items");
